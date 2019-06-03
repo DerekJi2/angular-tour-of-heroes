3806782a355935e7dcd025c74642330a5f7140b2
Derek Ji <syfool@hotmail.com>
2019-06-03 22:37:17 +0930

    Lesson 02. Fixes testing bugs

--- src/app/heroes/heroes.component.spec.ts
+++ src/app/heroes/heroes.component.spec.ts
@@ -1,10 +1,21 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Component, OnInit } from '@angular/core';
 import { HeroesComponent } from './heroes.component';
+import { FormsModule } from '@angular/forms';
 
 describe('HeroesComponent', () => {
   let component: HeroesComponent;
   let fixture: ComponentFixture<HeroesComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ HeroesComponent ],
+      imports: [
+        FormsModule
+      ]
+    })
+    .compileComponents();
+  }));
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
